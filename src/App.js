f94477b2ef5b9bf43887357157fd773f80c1e5bf
Sasha Shkapenko <sasha.shkapenko@gmail.com>
2022-05-25 22:00:52 +0100

    js-hw-1

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+import user from './user.json';
+import data from './data.json'
+import friends from './friends.json'
+import transactions from './transactions.json'
+import Profile from './components/Profile/Profile';
+import Statistics from './components/Statistics/Statistics';
+import FriendList from './components/FriendList/FriendList';
+import TransactionHistory from './components/TransactionHistory/TransactionHistory';
+
+export default function App() {
+  return (
+    <>
+      <Profile
+  username={user.username}
+  tag={user.tag}
+  location={user.location}
+  avatar={user.avatar}
+  stats={user.stats}
+    />
+    <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions}/>
+    </>
+  );
+}
+
+
+
+
---
+++ src/components/Statistics/Statistics.js
@@ -0,0 +1,30 @@
+import styles from './Statistics.module.css'
+import PropTypes from 'prop-types';
+export default function Statistics({title, stats}) {
+    return (
+        <section className={styles.statistics}>
+        {title && <h2 className={styles.title}>{title}</h2>}
+
+        <ul className={styles['stat-list']}>
+                {stats.map(({ id, label, percentage }) => {
+                    return (
+                        <li key={id} className={styles.item}>
+                            <span className={styles.label}>{label}</span>
+                            <span className={styles.percentage}>{percentage}%</span>
+                        </li>
+                    );
+            })}
+  </ul>
+</section>);
+};
+ 
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    }),
+  ),
+};
